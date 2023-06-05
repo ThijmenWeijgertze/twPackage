d641e215d9a8bf24c93bfe76f90580392db9970e
“Thijmen <thijmen.lifesciences@gmail.com>
2023-06-05 10:20:48 +0200

    adding doc folder

---
+++ doc/twPackage.R
@@ -0,0 +1,30 @@
+## ---- include = FALSE---------------------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>",
+  warning = FALSE,
+  message = FALSE
+)
+
+## ----examples-----------------------------------------------------------------
+# load package
+library(twPackage)
+
+## -----------------------------------------------------------------------------
+pokemon$Type.1 <- tw_factor(pokemon$Type.1)
+levels(pokemon$Type.1)
+
+## ---- eval=FALSE--------------------------------------------------------------
+#  tw_csv_rds(pokemon, path = "pokemon")
+
+## -----------------------------------------------------------------------------
+tw_table(pokemon, 5)
+
+## -----------------------------------------------------------------------------
+tw_filter_select(
+  pokemon, 
+  filter_col = "Type.1", 
+  filter_value = "Fire", 
+  select = c("Name", "Type.1")
+  ) %>% head(5)
+
